e9f28d2fc18ecb401fc321c5100fca8b4d1b33a8
Maria Clara <mariaclarafreirebarbosa@gmail.com>
2025-05-05 09:40:02 -0300

    Implementa login e cadastro, carrossel com autoplay e navegação por categorias

---
+++ pages/js/script.js
@@ -0,0 +1,26 @@
+let indice = 0;
+const slides = document.querySelectorAll(".slide");
+const indicadores = document.querySelectorAll(".indicadores span");
+
+function mostrarSlide(i) {
+  slides.forEach(slide => slide.classList.remove("ativo"));
+  indicadores.forEach(dot => dot.classList.remove("ativo"));
+  slides[i].classList.add("ativo");
+  indicadores[i].classList.add("ativo");
+  indice = i;
+}
+
+function avancarSlide() {
+  let novo = (indice + 1) % slides.length;
+  mostrarSlide(novo);
+}
+
+function voltarSlide() {
+  let novo = (indice - 1 + slides.length) % slides.length;
+  mostrarSlide(novo);
+}
+
+function mudarSlide(i) {
+  mostrarSlide(i);
+}
+
